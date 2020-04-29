e860fa1c7c311a6743a3a80a13cb8288120d2d9e
NickHackman <snickhackman@gmail.com>
2020-04-29 02:04:20 -0400

    Main entry point

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,6 +9,82 @@
     unused_must_use
 )]
 
+use clap::{crate_authors, crate_version, App, AppSettings, Arg, ArgMatches};
+
+use git_detective::{error::Error, GitDetective, Mode};
+
+fn cli() -> ArgMatches<'static> {
+    App::new("gd")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            App::new("clone")
+                .about("Clone a remote repository to inspect")
+                .alias("c")
+                .arg(
+                    Arg::with_name("path")
+                        .short("p")
+                        .long("path")
+                        .help("Path to clone to")
+                        .default_value("git_detective_repo"),
+                )
+                .arg(
+                    Arg::with_name("branch")
+                        .short("b")
+                        .help("Branch to checkout")
+                        .long("branch")
+                        .default_value("master"),
+                )
+                .arg(
+                    Arg::with_name("URL")
+                        .required(true)
+                        .help("Git URL to clone then open"),
+                ),
+        )
+        .subcommand(
+            App::new("open")
+                .about("Open a local repository to inspect")
+                .alias("o")
+                .arg(
+                    Arg::with_name("path")
+                        .default_value(".")
+                        .help("Path to Git repository to open"),
+                )
+                .arg(
+                    Arg::with_name("branch")
+                        .short("b")
+                        .help("Branch to checkout")
+                        .long("branch")
+                        .default_value("master"),
+                ),
+        )
+        .get_matches()
+}
+
+fn construct_git_detective(matches: ArgMatches) -> Result<GitDetective, Error> {
+    let branch = matches.value_of("branch").unwrap_or("master");
+    match matches.subcommand_name() {
+        Some("clone") => {
+            let path = matches.value_of("path").unwrap_or("git_detective_repo");
+            let uri = match matches.value_of("URL") {
+                Some(uri) => uri,
+                None => panic!("URL is required"),
+            };
+            GitDetective::new(Mode::Clone(path.to_string()), uri, branch)
+        }
+        Some("open") => {
+            let path = matches.value_of("path").unwrap_or(".");
+            GitDetective::new(Mode::Open, path, branch)
+        }
+        _ => panic!("Must enter a subcommand"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli();
+    let _git_detective = match construct_git_detective(matches) {
+        Ok(detective) => detective,
+        Err(e) => panic!("{:?}", e),
+    };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,55 @@
     unused_qualifications,
     unused_must_use
 )]
+
+/// Performs git operations
+mod git;
+use git::Repo;
+
+/// Errors for Git Detective
+pub mod error;
+use error::Error;
+
+/// Main Entry point
+pub struct GitDetective {
+    repo: Repo,
+}
+
+/// Mode for GitDetective
+pub enum Mode {
+    /// Open local git repository
+    Open,
+    /// Clone remote git repository
+    Clone(String),
+}
+
+impl GitDetective {
+    /// Construct a New instance of GitDetective
+    ///
+    /// # Parameters
+    ///
+    /// mode: Mode
+    ///
+    /// Either `Mode::Open` or `Mode::Clone` where Clone takes a path to clone to
+    ///
+    /// uri: S
+    ///
+    /// Either a URL or a Path to a directory
+    ///
+    /// branch: &str
+    ///
+    /// Branch to checkout when opening
+    ///
+    /// # Returns
+    ///
+    /// Result<GitDetective, Error>
+    pub fn new<S: Into<String>>(mode: Mode, uri: S, branch: &str) -> Result<Self, Error> {
+        let uri = uri.into();
+        let repo = match mode {
+            Mode::Open => Repo::open(uri)?,
+            Mode::Clone(path) => Repo::clone(&uri, path)?,
+        };
+        repo.checkout(branch)?;
+        Ok(Self { repo })
+    }
+}
