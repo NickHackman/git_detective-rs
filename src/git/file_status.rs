a3f28913b27b672830bb1be46e4cbbb26056466e
NickHackman <snickhackman@gmail.com>
2020-05-16 16:14:13 -0400

    Added Rust 2018 Idioms lint
    Enforce better practices for the project as a whole

--- src/git/file_status.rs
+++ src/git/file_status.rs
@@ -10,7 +10,7 @@ pub struct FileStatus {
 }
 
 impl<'repo> From<git2::StatusEntry<'repo>> for FileStatus {
-    fn from(status_entry: git2::StatusEntry) -> Self {
+    fn from(status_entry: git2::StatusEntry<'_>) -> Self {
         let path = String::from_utf8_lossy(status_entry.path_bytes()).to_string();
         Self {
             path,
