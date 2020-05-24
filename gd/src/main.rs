87e80159517b34c2ae5219f1503eaa01826b2059
NickHackman <snickhackman@gmail.com>
2020-05-23 21:43:58 -0400

    Added final_contributions table printing
    Prints out a final contributions table split by contributor and by
    language very similar to `Tokei`, but doesn't scale for terminal size at
    the moment.

--- gd/src/main.rs
+++ gd/src/main.rs
@@ -1,6 +1,6 @@
 //! Git Detective
 //!
-//! A Terminal User Interface to view git contributions
+//! A Command line application to better inspect contributions in a Git Repository
 #![deny(
     missing_docs,
     unsafe_code,
@@ -9,6 +9,8 @@
     unused_must_use
 )]
 
+use std::process;
+
 use clap::ArgMatches;
 
 use git_detective::{Error, GitDetective};
@@ -16,28 +18,71 @@ use git_detective::{Error, GitDetective};
 mod cli;
 use cli::clap;
 
-fn construct_git_detective(matches: ArgMatches) -> Result<GitDetective, Error> {
-    if let Some(clone) = matches.subcommand_matches("clone") {
-        let path = matches.value_of("path").unwrap_or("git_detective_repo");
-        GitDetective::clone(
-            clone.value_of("repository").unwrap(),
-            path,
-            clone.is_present("recursive"),
-        )
-    } else if let Some(open) = matches.subcommand_matches("open") {
-        let path = open.value_of("repository").unwrap_or(".");
-        GitDetective::open(path)
-    } else {
-        panic!("Must enter a subcommand")
+mod util;
+use util::final_table;
+
+fn construct_gd(matches: &ArgMatches) -> Result<GitDetective, Error> {
+    let gd = match matches.subcommand() {
+        ("clone", Some(c_matches)) => GitDetective::clone(
+            c_matches.value_of("repository").unwrap(),
+            c_matches.value_of("path").unwrap(),
+            c_matches.is_present("recursive"),
+        )?,
+        _ => GitDetective::open(".")?,
+    };
+    Ok(gd)
+}
+
+fn run(matches: ArgMatches) -> Result<(), Error> {
+    let mut gd = construct_gd(&matches)?;
+    match matches.subcommand() {
+        ("list", Some(list_args)) => Ok(list(list_args, &gd)?),
+        ("statistics", Some(stats_args)) => Ok(stats(stats_args, &mut gd)?),
+        ("clone", _) => Ok(()),
+        ("checkout", Some(checkout_args)) => Ok(checkout(checkout_args, &gd)?),
+        _ => unreachable!(),
     }
+}
+
+fn stats(matches: &ArgMatches, gd: &mut GitDetective) -> Result<(), Error> {
+    let _name = matches.value_of("name");
+    if matches.is_present("final") {
+        let final_contribs = gd.final_contributions()?;
+        final_table(final_contribs);
+    }
+    if matches.is_present("files") {
+        let files = gd.files_contributed_to()?;
+        println!("{:#?}", files);
+    }
+    if matches.is_present("difference") {
+        let diff_stats = gd.diff_stats()?;
+        println!("{:#?}", diff_stats);
+    }
+    Ok(())
+}
+
+fn list(matches: &ArgMatches, _gd: &GitDetective) -> Result<(), Error> {
+    // TODO: call functions
+    if matches.is_present("commits") {}
+    if matches.is_present("tags") {}
+    if matches.is_present("contributors") {}
+    if matches.is_present("files") {}
+    if matches.is_present("branches") {}
+    Ok(())
+}
+
+// TODO: implement
+fn checkout(matches: &ArgMatches, _gd: &GitDetective) -> Result<(), Error> {
+    Ok(())
 }
 
 fn main() {
     let matches = clap();
-    let gd = match construct_git_detective(matches) {
-        Ok(detective) => detective,
-        Err(e) => panic!("{:?}", e),
-    };
-    let contributions = gd.final_contributions().unwrap();
-    println!("{:?}", contributions);
+    match run(matches) {
+        Ok(_) => process::exit(0),
+        Err(e) => {
+            println!("{}", e);
+            process::exit(-1);
+        }
+    }
 }
---
+++ gd/src/util/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod final_contributions;
+pub use final_contributions::{line_separator, table as final_table};
